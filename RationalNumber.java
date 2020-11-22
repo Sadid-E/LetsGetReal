3bd75d1047616523b1a1239726b11ddb7e7642c6
Sadid-E <>
2020-11-22 01:47:49 -0500

    Added the constructor and getValue method

---
+++ RationalNumber.java
@@ -0,0 +1,19 @@
+public class RationalNumber extends RealNumber{
+  private int numerator, denominator;
+
+  /**Initialize the RationalNumber with the provided values
+  *  if the denominator is 0, make the fraction 0/1 instead
+  *@param nume the numerator
+  *@param deno the denominator
+  */
+  public RationalNumber(int nume, int deno){
+    super(0.0);
+    numerator = nume;
+    denominator = deno;
+  }
+
+  public double getValue(){
+    return ((double) numerator / (double) denominator);
+  }
+
+}
